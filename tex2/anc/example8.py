2aa5109ce3a2d181b6bb66de4c4b1e2530f62d40
Phillip Weinberg <weinbe58@bu.edu>
2017-05-17 15:18:01 -0400

    boson section almost finished.

--- tex2/anc/example8.py
+++ tex2/anc/example8.py
@@ -33,9 +33,9 @@ combination of two ladder parity operators!
 
 ladder parity operators to come soon!
 """
-# initial see for random number generator
+##### initial see for random number generator
 np.random.seed(0) # seed is 0 to produce plots from QuSpin2 paper
-# setting up parameters of simulation
+##### setting up parameters of simulation
 L = 6 # length of chain
 N = 2*L # number of sites
 nb = 0.5 # density of bosons
@@ -44,18 +44,18 @@ J_par_1 = 1.0 # top side of ladder hopping
 J_par_2 = 1.0 # bottom side of ladder hopping
 J_perp =  0.5 # rung hopping
 U = 10.0 # Hubbard interaction
-# setting up parameters for evolution
+##### setting up parameters for evolution
 start,stop,num = 0,30,301 # 0.1 equally spaced points
 times = np.linspace(start,stop,num)
-# U n_i(n_i-1) interaction
+##### U n_i(n_i-1) interaction
 int_list_2 = [[U,i,i] for i in range(N)] # U n_i^2
 int_list_1 = [[-U,i] for i in range(N)] # -U n_i
-# setting up hopping lists
+##### setting up hopping lists
 hop_list = [[-J_par_1,i,(i+2)%N] for i in range(0,N,2)] # PBC bottom 
 hop_list.extend([[-J_par_2,i,(i+2)%N] for i in range(1,N,2)]) # PBC top
 hop_list.extend([[-J_perp,i,i+1] for i in range(0,N,2)]) # perp hopping
 hop_list_hc = [[J.conjugate(),i,j] for J,i,j in hop_list]
-# setting up static list
+##### setting up static list
 static = [
 			["+-",hop_list], # hopping
 			["-+",hop_list_hc], # hopping h.c.
@@ -72,6 +72,10 @@ U_block = block_ops(blocks,static,dynamic,boson_basis_1d,baisis_args,np.complex1
 					basis_kwargs=basis_kwargs,get_proj_kwargs=get_proj_kwargs)
 ##### setting up basis for local fock basis
 basis = boson_basis_1d(N,nb=nb,sps=sps)
+##### setting up observables
+sub_sys_A = range(0,N,2) # bottom side of ladder 
+no_checks = dict(check_herm=False,check_symm=False,check_pcon=False)
+n_list = [hamiltonian([["n",[[1.0,i]]]],[],basis=basis,dtype=np.float64,**no_checks) for i in range(N)]
 ##### set up initial state
 i0 = np.random.randint(basis.Ns) # pick random state from basis set
 psi = np.zeros(basis.Ns,dtype=np.float64)
@@ -80,15 +84,12 @@ psi[i0] = 1.0
 state_str = "".join(str(int((basis[i0]//basis.sps**i)%basis.sps)) for i in range(N))
 print("total H-space size: {}, initial state: |{}>".format(basis.Ns,state_str))
 ##### calculating the evolved states
-n_jobs = 4 # increase this to see if calculation runs faster!
+n_jobs = 1 # increase this to see if calculation runs faster!
 psi_t = U_block.expm(psi,start=start,stop=stop,num=num,block_diag=False,n_jobs=n_jobs)
-##### setting up observables
-sub_sys_A = range(0,N,2) # bottom side of ladder 
-no_checks = dict(check_herm=False,check_symm=False,check_pcon=False)
-n_list = [hamiltonian([["n",[[1.0,i]]]],[],basis=basis,dtype=np.float64,**no_checks) for i in range(N)]
 ##### calculating entanglement entropy 
-ent_gen = (basis.ent_entropy(psi,sub_sys_A=sub_sys_A)["Sent_A"]/L for psi in psi_t.T[:])
-ent_t = np.fromiter(ent_gen,dtype=np.float64,count=num)
+gen = (basis.ent_entropy(psi,sub_sys_A=sub_sys_A)["Sent_A"]/L for psi in psi_t.T[:])
+ent_t = np.fromiter(gen,dtype=np.float64,count=num)
+##### calculating the local densities as a function of time
 expt_n_t = np.vstack([n.expt_value(psi_t).real for n in n_list]).T
 # reshape data for plotting
 n_t = np.zeros((num,2,L))

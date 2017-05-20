5b6439926153dbd8e73597efd54dcf7f6d702018
Phillip Weinberg <weinbe58@bu.edu>
2017-05-20 17:44:00 -0400

    added MBL example

--- tex2/anc/example8.py
+++ tex2/anc/example8.py
@@ -33,9 +33,9 @@ combination of two ladder parity operators!
 
 ladder parity operators to come soon!
 """
-##### initial see for random number generator
+# initial see for random number generator
 np.random.seed(0) # seed is 0 to produce plots from QuSpin2 paper
-##### setting up parameters of simulation
+# setting up parameters of simulation
 L = 6 # length of chain
 N = 2*L # number of sites
 nb = 0.5 # density of bosons
@@ -44,18 +44,18 @@ J_par_1 = 1.0 # top side of ladder hopping
 J_par_2 = 1.0 # bottom side of ladder hopping
 J_perp =  0.5 # rung hopping
 U = 10.0 # Hubbard interaction
-##### setting up parameters for evolution
+# setting up parameters for evolution
 start,stop,num = 0,30,301 # 0.1 equally spaced points
 times = np.linspace(start,stop,num)
-##### U n_i(n_i-1) interaction
+# U n_i(n_i-1) interaction
 int_list_2 = [[U,i,i] for i in range(N)] # U n_i^2
 int_list_1 = [[-U,i] for i in range(N)] # -U n_i
-##### setting up hopping lists
+# setting up hopping lists
 hop_list = [[-J_par_1,i,(i+2)%N] for i in range(0,N,2)] # PBC bottom 
 hop_list.extend([[-J_par_2,i,(i+2)%N] for i in range(1,N,2)]) # PBC top
 hop_list.extend([[-J_perp,i,i+1] for i in range(0,N,2)]) # perp hopping
 hop_list_hc = [[J.conjugate(),i,j] for J,i,j in hop_list]
-##### setting up static list
+# setting up static list
 static = [
 			["+-",hop_list], # hopping
 			["-+",hop_list_hc], # hopping h.c.
@@ -63,33 +63,33 @@ static = [
 			["n",int_list_1] # -U n_i
 		]
 dynamic = [] # no dynamic operators
-##### creating block_ops object
+# creating block_ops object
 blocks=[dict(kblock=kblock) for kblock in range(L)] # blocks to project on to
 baisis_args = (N,) # boson_basis_1d manditory arguments
 basis_kwargs = dict(nb=nb,sps=sps,a=2) # boson_basis_1d optional args
 get_proj_kwargs = dict(pcon=True) # set projection to full particle basis
 U_block = block_ops(blocks,static,dynamic,boson_basis_1d,baisis_args,np.complex128,
 					basis_kwargs=basis_kwargs,get_proj_kwargs=get_proj_kwargs)
-##### setting up basis for local fock basis
+# setting up basis for local fock basis
 basis = boson_basis_1d(N,nb=nb,sps=sps)
-##### setting up observables
+# setting up observables
 sub_sys_A = range(0,N,2) # bottom side of ladder 
 no_checks = dict(check_herm=False,check_symm=False,check_pcon=False)
 n_list = [hamiltonian([["n",[[1.0,i]]]],[],basis=basis,dtype=np.float64,**no_checks) for i in range(N)]
-##### set up initial state
+# set up initial state
 i0 = np.random.randint(basis.Ns) # pick random state from basis set
 psi = np.zeros(basis.Ns,dtype=np.float64)
 psi[i0] = 1.0
-##### print info about setup
+# print info about setup
 state_str = "".join(str(int((basis[i0]//basis.sps**i)%basis.sps)) for i in range(N))
 print("total H-space size: {}, initial state: |{}>".format(basis.Ns,state_str))
-##### calculating the evolved states
+# calculating the evolved states
 n_jobs = 1 # increase this to see if calculation runs faster!
 psi_t = U_block.expm(psi,start=start,stop=stop,num=num,block_diag=False,n_jobs=n_jobs)
-##### calculating entanglement entropy 
+# calculating entanglement entropy 
 gen = (basis.ent_entropy(psi,sub_sys_A=sub_sys_A)["Sent_A"]/L for psi in psi_t.T[:])
 ent_t = np.fromiter(gen,dtype=np.float64,count=num)
-##### calculating the local densities as a function of time
+# calculating the local densities as a function of time
 expt_n_t = np.vstack([n.expt_value(psi_t).real for n in n_list]).T
 # reshape data for plotting
 n_t = np.zeros((num,2,L))
--- tex2/anc/example9.py
+++ tex2/anc/example9.py
@@ -1,117 +1,105 @@
 from __future__ import print_function, division
-from quspin.operators import hamiltonian,exp_op,ops_dict # Hamiltonians and other operators
-from quspin.basis import tensor_basis,fermion_basis_1d # fermion and tensor Hilbert spaces
-from quspin.tools.measurements import obs_vs_time # function to calculating dynamics
+from quspin.operators import hamiltonian,exp_op,ops_dict # operators
+from quspin.basis import tensor_basis,fermion_basis_1d # Hilbert spaces
+from quspin.tools.measurements import obs_vs_time # calculating dynamics
 import numpy as np # general math functions
 from numpy.random import uniform,choice # tools for doing random sampling
-from joblib import Parallel,delayed # tools for doing
 from time import time # tool for calculating computation time
 import matplotlib.pyplot as plt # plotting
-# setting parameters for simulation
-n_jobs = 2 # number of cores to use in calculating realizations
-n_real = 1000 # number of realizations
-n_boot = 100*n_real # number of bootstrap realizations to calculate error
+##### setting parameters for simulation ####
+# simulation parameters
+n_real = 100 # number of realizations
+n_boot = 100 # number of bootstrap samples to calculate error
 # physical parameters
 L = 8 # system size
 N = L//2 # number of particles
-w1 = 1.0 # disorder strength
-w2 = 4.0
-w3 = 10.0 
+w_list = [1.0,4.0,10.0] # disorder strength
 J = 1.0 # hopping strength
 U = 5.0 # interaction strength
-k = 0.1 # trap stiffness
 # range to evolve system
 start,stop,num=0.0,35.0,101
+times = np.linspace(start,stop,num=num,endpoint=True)
 # setting up basis
 N_up = N//2 + N % 2 # number of fermions with spin up
 N_down = N//2 # number of fermions with spin down
-# building the two basis to tensor together
+##### building the two basis to tensor together
 basis_up = fermion_basis_1d(L,Nf=N_up) # up basis
 basis_down = fermion_basis_1d(L,Nf=N_down) # down basis
 basis = tensor_basis(basis_up,basis_down) # spinful fermsions
+##### setting up physical problem
 # creating coupling lists
-i_mid = (L//2+1 if L%2 else L//2+0.5) # mid point on lattice
 hop_right = [[-J,i,i+1] for i in range(L-1)] # hopping to the right OBC
 hop_left = [[J,i,i+1] for i in range(L-1)] # hopping to the left OBC
 int_list = [[U,i,i] for i in range(L)] # onsite interaction
-trap_list = [[0.5*k*(i-i_mid)**2,i] for i in range(L)] # harmonic trap
 # coupling list to create the sublattice imbalance observable
-sublat_list = [[(-1)**i,i] for i in range(0,L)]
+sublat_list = [[(-1)**i/N,i] for i in range(0,L)]
 # create static lists
 operator_list_0 = [	
 			["+-|", hop_left], # up hop left
 			["-+|", hop_right], # up hop right
-			["n|", trap_list], # up trap potential
 			["|+-", hop_left], # down hop left
 			["|-+", hop_right], # down hop right
-			["|n", trap_list], # down trap potential
 			["n|n", int_list], # onsite interaction
-		 ]
+			]
 # create operator dictionary for ops_dict class
 # creates a dictioanry with keys h0,h1,h2,...,hL for local potential
-operator_dict = {"h"+str(i):[["n|",[[1.0,i]]],["|n",[[1.0,i]]]] for i in range(L)}
-operator_dict["H0"]=operator_list_0
+# add key for Hubbard hamiltonian
+operator_dict=dict(H0=operator_list_0)
+# add keys for local potential in each site
+for i in range(L):
+	operator_dict["n"+str(i)] = [["n|",[[1.0,i]]],["|n",[[1.0,i]]]]
+###### setting up operators	
 # set up hamiltonian dictionary and observable
 no_checks = dict(check_pcon=False,check_symm=False,check_herm=False)
 H_dict = ops_dict(operator_dict,basis=basis,**no_checks)
-I = hamiltonian([["n|",sublat_list],["|n",sublat_list]],[],basis=basis,**no_checks)/N
+I = hamiltonian([["n|",sublat_list],["|n",sublat_list]],[],basis=basis,**no_checks)
 # strings which represent the initial state
 s_up = "".join("1000" for i in range(N_up))
 s_down = "".join("0010" for i in range(N_down))
-print("H-space size: {:d}, initial state: |{:s}>(x)|{:s}>".format(basis.Ns,s_up,s_down))
 # basis.index accepts strings and returns the index 
 # which corresponds to that state in the basis list
-i_0 = basis.index(s_up,s_down)
-psi_0 = np.zeros(basis.Ns)
-psi_0[i_0] = 1.0
-# set up times
-times = np.linspace(start,stop,num=num,endpoint=True)
+i_0 = basis.index(s_up,s_down) # find index of product state
+psi_0 = np.zeros(basis.Ns) # allocate space for state
+psi_0[i_0] = 1.0 # set state to be in the given product state
+print("H-space size: {:d}, initial state: |{:s}>(x)|{:s}>".format(basis.Ns,s_up,s_down))
 # define function to do dynamics for different disorder realizations.
-def realization(H_dict,I,psi_0,disorder,start,stop,num,i):
+def real(H_dict,I,psi_0,w,times,i):
+	# body of function goes below
 	ti = time() # start timing function
 	# create a parameter list which specifies the onsite potential with disorder
-	parameters = {"h"+str(i):h for i,h in enumerate(disorder)}
+	params=dict(H0=1)
+	for i in range(L):
+		params["n"+str(i)] = uniform(-w,w)
 	# using the parameters dictionary construct a hamiltonian object with those
 	# parameters defined in the list
-	H = H_dict.tohamiltonian(parameters)
+	H = H_dict.tohamiltonian(params)
 	# use exp_op to get the evolution operator
-	U = exp_op(H,a=-1j,start=start,stop=stop,num=num,endpoint=True,iterate=True)
+	U = exp_op(H,a=-1j,start=times.min(),stop=times.max(),num=len(times),iterate=True)
 	psi_t = U.dot(psi_0) # get generator of time evolution
 	# use obs_vs_time to evaluate the dynamics
-	obs_t = obs_vs_time(psi_t,U.grid,dict(I=I))
+	times = U.grid # time grid stored in U
+	obs_t = obs_vs_time(psi_t,times,dict(I=I))
 	# print reporting the computation time for realization
 	print("realization {}/{} completed in {:.2f} s".format(i+1,n_real,time()-ti))
 	# return observable values.
 	return obs_t["I"]
-# machinery for doing parallel realizations loop
-I_data_1 = np.vstack(Parallel(n_jobs=n_jobs)(delayed(realization)(H_dict,I,psi_0,uniform(-w1,w1,size=L),start,stop,num,i) for i in range(n_real)))
-I_data_2 = np.vstack(Parallel(n_jobs=n_jobs)(delayed(realization)(H_dict,I,psi_0,uniform(-w2,w2,size=L),start,stop,num,i) for i in range(n_real)))
-I_data_3 = np.vstack(Parallel(n_jobs=n_jobs)(delayed(realization)(H_dict,I,psi_0,uniform(-w3,w3,size=L),start,stop,num,i) for i in range(n_real)))
-# calculating mean and error via bootstrap sampling
-I_1 = I_data_1.mean(axis=0) # get mean value of I
-I_2 = I_data_2.mean(axis=0) 
-I_3 = I_data_3.mean(axis=0) 
-# generate bootstrap samples
-bootstrap_gen_1 = (I_data_1[choice(n_real,n_real)].mean(axis=0) for i in range(n_boot)) 
-bootstrap_gen_2 = (I_data_2[choice(n_real,n_real)].mean(axis=0) for i in range(n_boot)) 
-bootstrap_gen_3 = (I_data_3[choice(n_real,n_real)].mean(axis=0) for i in range(n_boot)) 
-# generate the fluctuations about the mean of I
-sq_fluc_gen_1 = ((bootstrap-I_1)**2 for bootstrap in bootstrap_gen_1) 
-sq_fluc_gen_2 = ((bootstrap-I_2)**2 for bootstrap in bootstrap_gen_2)
-sq_fluc_gen_3 = ((bootstrap-I_3)**2 for bootstrap in bootstrap_gen_3) 
-# error is calculated as the squareroot of mean fluctuations
-dI_1 = 2.0*np.sqrt(sum(sq_fluc_gen_1)/n_boot) 
-dI_2 = 2.0*np.sqrt(sum(sq_fluc_gen_2)/n_boot) 
-dI_3 = 2.0*np.sqrt(sum(sq_fluc_gen_3)/n_boot) 
-# plot imbalance with error bars
-fig = plt.figure()
+###### looping over differnt disorder strengths
+for w in w_list:	
+	I_data = np.vstack([real(H_dict,I,psi_0,w,times,i) for i in range(n_real)])
+	##### averaging and error estimation
+	I_avg = I_data.mean(axis=0) # get mean value of I for all time points
+	# generate bootstrap samples
+	bootstrap_gen = (I_data[choice(n_real,size=n_real)].mean(axis=0) for i in range(n_boot)) 
+	# generate the fluctuations about the mean of I
+	sq_fluc_gen = ((bootstrap-I_avg)**2 for bootstrap in bootstrap_gen)
+	I_error = np.sqrt(sum(sq_fluc_gen)/n_boot) 
+	# plotting results
+	plt.errorbar(times,I_avg,I_error,marker=".",label="w={:.2f}".format(w))
+##### configuring plots
 plt.xlabel("$t/J$",fontsize=18)
 plt.ylabel("$\mathcal{I}$",fontsize=18)
 plt.grid(True)
 plt.tick_params(labelsize=16)
-plt.errorbar(times,I_1,dI_1,marker=".",label="w={:.2f}".format(w1))
-plt.errorbar(times,I_2,dI_2,marker=".",label="w={:.2f}".format(w2))
-plt.errorbar(times,I_3,dI_3,marker=".",label="w={:.2f}".format(w3))
 plt.legend(loc=0)
-fig.savefig('fermion_MBL.pdf', bbox_inches='tight')
+plt.savefig('fermion_MBL.pdf', bbox_inches='tight')
 plt.show()
